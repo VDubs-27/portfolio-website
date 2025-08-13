9fa3012820a50227eb7364e865337e5486809fbd
Vikram Varadarajan <139834292+27VDubs@users.noreply.github.com>
2025-08-13 19:21:40 +1200

    Added CSS for Introduction, need to make button responsive

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -2,7 +2,7 @@ export default function Introduction() {
     return (
         <div className="introduction">
             <div className="pfp">
-                <img src="https://avatars.githubusercontent.com/u/12345678?v=4" alt="Profile" />
+                <img src="https://avatars.githubusercontent.com/u/188545134?v=4" alt="Profile" />
             </div>
             <h1>Vikram Varadarajan</h1>
             <p>Software Engineering Student</p>
