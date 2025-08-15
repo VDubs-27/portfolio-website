99576709aed52dd6e9b1703ab8c50e20010c1c70
Vikram Varadarajan <139834292+27VDubs@users.noreply.github.com>
2025-08-15 23:22:08 +1200

    Added conditional rendering for paragraph text to change on profile image click

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default function Introduction() {
 
     const [curtainsOpen, setCurtainsOpen] = React.useState(false);
-
     function openCurtains() {
         setCurtainsOpen(true);
         document.querySelector(".enter").style.animation = 'none';
@@ -15,11 +14,14 @@ export default function Introduction() {
 
     function handleImageClick() {
         const img = document.querySelector(".pfp img");
+        const textElement = document.querySelector(".text");
         // Ternary operator based on the click
         img.classList.contains('flipped') ? img.classList.remove('flipped') : img.classList.add('flipped');
 
+        img.classList.contains('flipped') ? textElement.textContent = "📍 Auckland, New Zealand" : textElement.textContent = "🎓 Software Engineering Student";
+
         setTimeout(() => {
-            img.classList.contains('flipped') ? img.src = 'https://upload.wikimedia.org/wikipedia/commons/d/da/Venkateswara_namaalu.png' : img.src = 'https://avatars.githubusercontent.com/u/188545134?v=4';
+            img.classList.contains('flipped') ? img.src = 'https://images.pexels.com/photos/29015656/pexels-photo-29015656/free-photo-of-sky-tower-and-auckland-harbor-view.jpeg' : img.src = 'https://avatars.githubusercontent.com/u/188545134?v=4';
         }, 200);
     }
 
@@ -32,7 +34,7 @@ export default function Introduction() {
                     <img src="https://avatars.githubusercontent.com/u/188545134?v=4" alt="Profile" onClick={handleImageClick}/>
                 </div>
                 <h1 className="name">Vikram Varadarajan</h1>
-                <p>Software Engineering Student</p>
+                <p className="text">🎓 Software Engineering Student</p>
                 <button className="enter" onClick={openCurtains}>Go</button>
             </div>
         </>
