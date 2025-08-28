479078303f309528560ea2fd2cbb316a539335da
Vikram Varadarajan <139834292+27VDubs@users.noreply.github.com>
2025-08-28 22:32:43 +1200

    Made changes to image file in segment1

--- src/components/Segment1.jsx
+++ src/components/Segment1.jsx
@@ -3,7 +3,7 @@ export default function Segment1() {
         <div className="segment1">
             {/* <h1 className="segment1-heading">Hi, I'm Vikram Varadarajan</h1>
             <p>Part II Software Engineering Student at the University of Auckland</p> */}
-            {/* <img className="profile-img" src="/src/assets/landingimg.png" alt="Profile" /> */}
+            <img className="profile-img" src="/src/assets/landingimg.png" alt="Profile" style={{ opacity: 0.8 }} />
         </div>
         
     )
