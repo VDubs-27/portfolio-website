c59862ad8fc9d5ab11cfb875a2e69725e9ef1601
Vikram Varadarajan <139834292+27VDubs@users.noreply.github.com>
2025-08-29 11:15:57 +1200

    Added introductory text in segment1 and styling for it

--- src/components/Segment1.jsx
+++ src/components/Segment1.jsx
@@ -1,8 +1,12 @@
 export default function Segment1() {
     return (
         <div className="segment1">
-            {/* <h1 className="segment1-heading">Hi, I'm Vikram Varadarajan</h1>
-            <p>Part II Software Engineering Student at the University of Auckland</p> */}
+            <div className="segment1-content">
+                <h1 className="segment1-heading">Hi, I'm Vikram Varadarajan</h1>
+                <h3 className="subtitle">Software Engineering Student at the University of Auckland</h3>
+                <button className="projects-button"><a href="#projects">View Projects</a></button>
+                <button className="contact-button"><a href="#contact">Contact Me</a></button>
+            </div>
             <img className="profile-img" src="/src/assets/landingimg.png" alt="Profile" style={{ opacity: 0.8 }} />
         </div>
         
