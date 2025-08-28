5a08a732c60c018f2a610ed3583fcba633840e2c
Vikram Varadarajan <139834292+27VDubs@users.noreply.github.com>
2025-08-29 11:58:21 +1200

    Added icons with styling to LinkedIn, GitHub, and Email

--- src/components/Segment1.jsx
+++ src/components/Segment1.jsx
@@ -6,6 +6,11 @@ export default function Segment1() {
                 <h3 className="subtitle">Software Engineering Student at the University of Auckland</h3>
                 <button className="projects-button"><a href="#projects">View Projects</a></button>
                 <button className="contact-button"><a href="#contact">Contact Me</a></button>
+                <div className="external-links">
+                    <img src="/src/assets/github.png" alt="GitHub" onClick={() => window.open("https://github.com/VDubs-27", "_blank")} />
+                    <img src="/src/assets/linkedin.png" alt="LinkedIn" onClick={() => window.open("https://www.linkedin.com/in/vikram-varadarajan-54aaa6283/", "_blank")} />
+                    <img src="/src/assets/email.png" alt="Email" onClick={() => window.open("mailto:vikramaditya.varadarajan@gmail.com", "_blank")} />
+                </div>
             </div>
             <img className="profile-img" src="/src/assets/landingimg.png" alt="Profile" style={{ opacity: 0.8 }} />
         </div>
