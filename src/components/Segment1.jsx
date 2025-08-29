acdb5d614cfc5d1ea56a239cc049a5be252de52b
Vikram Varadarajan <139834292+27VDubs@users.noreply.github.com>
2025-08-29 12:07:50 +1200

    Fixed bug with button links only functioning on text click

--- src/components/Segment1.jsx
+++ src/components/Segment1.jsx
@@ -4,8 +4,9 @@ export default function Segment1() {
             <div className="segment1-content">
                 <h1 className="segment1-heading">Hi, I'm Vikram Varadarajan</h1>
                 <h3 className="subtitle">Software Engineering Student at the University of Auckland</h3>
-                <button className="projects-button"><a href="#projects">View Projects</a></button>
-                <button className="contact-button"><a href="#contact">Contact Me</a></button>
+                <button className="projects-button" onClick={() => window.location.href="#projects"}>View Projects</button>
+                <button className="contact-button" onClick={() => window.location.href="#contact"}>Contact Me</button>
+                <button className="contact-button" onClick={() => window.open("https://drive.google.com/file/d/1lxd5_QUTBv7C9NAuPfiNbJFf_AhZYAp8/view?usp=drive_link", "_blank")}>View Resume</button>
                 <div className="external-links">
                     <img src="/src/assets/github.png" alt="GitHub" onClick={() => window.open("https://github.com/VDubs-27", "_blank")} />
                     <img src="/src/assets/linkedin.png" alt="LinkedIn" onClick={() => window.open("https://www.linkedin.com/in/vikram-varadarajan-54aaa6283/", "_blank")} />
