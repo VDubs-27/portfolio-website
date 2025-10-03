4161d748effbd4ce8f25d32b41324696352de88f
Vikram Varadarajan <vikramaditya.varadarajan@gmail.com>
2025-10-03 13:00:38 +1300

    Added contact modal as the final section to the webpage and included scrolling for buttons on segment1

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Scroll from "./components/Scroll"
 import About from "./components/About"
 import Network from "./components/Network"
 import Projects from "./components/Projects"
+import Contact from "./components/Contact"
 
 function App() {
 
@@ -38,6 +39,7 @@ function App() {
             <About />
             <Projects />
             <Network />
+            <Contact />
           </div>
           <Footer />
         </div>
--- src/components/Segment1.jsx
+++ src/components/Segment1.jsx
@@ -1,4 +1,14 @@
+import Projects from "./Projects";
+
 export default function Segment1() {
+    const handleScroll = (e, targetId) => {
+        e.preventDefault();
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+
     return (
         <div className="segment1">
             <div className="segment1-content">
@@ -10,9 +20,9 @@ export default function Segment1() {
                         <span style={{"animation": "change 2s infinite ease-in-out, text-change 10s infinite ease-in-out", "fontSize": "2.7rem", "fontWeight": "bold", "color": "white", "margin-top" : "3px"}}>committed</span>
                     </span>
                     </div> in building <span style={{"fontSize": "2.2rem", "color": "white", "fontWeight": "bold"}}>high-quality</span> software that people will <span style={{"fontSize": "2rem", "color": "white","fontWeight": "bold"}}>value</span></h3>
-                <button className="projects-button" onClick={() => window.location.href="#projects"}>View Projects</button>
-                <button className="contact-button" onClick={() => window.location.href="#contact"}>Contact Me</button>
-                <button className="contact-button" onClick={() => window.open("https://drive.google.com/file/d/1lxd5_QUTBv7C9NAuPfiNbJFf_AhZYAp8/view?usp=drive_link", "_blank")}>View Resume</button>
+                <button className="projects-button" onClick={(e) => handleScroll(e, "projects")}>View Projects</button>
+                <button className="contact-button" onClick={(e) => handleScroll(e, "contact")}>Contact Me</button>
+                <button className="resume-button" onClick={() => window.open("https://drive.google.com/file/d/1lxd5_QUTBv7C9NAuPfiNbJFf_AhZYAp8/view?usp=drive_link", "_blank")}>View Resume</button>
                 <div className="external-links">
                     <img src="/src/assets/github.png" alt="GitHub" onClick={() => window.open("https://github.com/VDubs-27", "_blank")} />
                     <img src="/src/assets/linkedin.png" alt="LinkedIn" onClick={() => window.open("https://www.linkedin.com/in/vikram-varadarajan-54aaa6283/", "_blank")} />
